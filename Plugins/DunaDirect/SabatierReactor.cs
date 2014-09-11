840cb14c76a106cd4f9ee2e5f3042ed7543264c0
jacob berkman <jacob@87k.net>
2014-09-10 23:09:56 -0700

    Initial plugin project

---
+++ Plugins/DunaDirect/SabatierReactor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class SabatierReactor : ModuleGenerator {
+
+    // Plugin Loaded
+    public override void OnStart(StartState state) {
+        print("Starting SabatierReactor");
+        base.OnStart(state);
+    }
+
+    public override void OnFixedUpdate() {
+        if (generatorIsActive) {
+            foreach (GeneratorResource resource in inputList) {
+                print("checking resource '" + resource.name + "': " + resource.isDeprived);
+                if (resource.isDeprived) {
+                    print("a resource is deprived: " + resource.name);
+                    Shutdown();
+                    break;
+                }
+            }
+        }
+        base.OnFixedUpdate();
+    }
+
+}
